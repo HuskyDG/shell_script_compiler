807d6e3b7cba44358e780a808240b68120cc3106
shìwēi nguyen <84650617+HuskyDG@users.noreply.github.com>
2023-06-11 18:55:17 +0700

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,1 +1,7 @@
-echo Hello. You have executed $ORIGINAL_PATH 
+# Note, $0 will be /proc/self/fd/<number> after compiling
+# Use $ORIGINAL_PATH to refer to execute binary path
+
+WELCOME="Hello. You have executed $ORIGINAL_PATH or $0"
+echo $WELCOME
+echo $WELCOME | base64
+
