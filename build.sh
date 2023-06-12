7764b9df174258e5218bee632a3b646d294865b5
Takeda-senpai <deadboltsx303@gmail.com>
2023-06-12 19:50:40 +0700

    package multiple scripts

--- build.sh
+++ build.sh
@@ -5,9 +5,19 @@ set -e
 
 export PATH=${PATH}:${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin
 
+SCRIPT_LIST="
+scripts/main.sh
+"
+
+for script in $SCRIPT_LIST; do
+    if ! bash -n "$script"; then
+        exit 2
+    fi
+done
+
 cat <<EOF >./native/jni/script.hpp
 #include "obfuscate.h"
-const char *shell_script = OBFUSCATE("$(cat ./scripts/script.sh | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' | sed 's/$/\\n/g' | tr -d '\n')");
+const char *shell_script = OBFUSCATE("$(( for script in $SCRIPT_LIST; do cat "$script"; echo ""; done; ) | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' | sed 's/$/\\n/g' | tr -d '\n')");
 EOF
 
 CXX=${CXX:-aarch64-linux-android31-clang++}
